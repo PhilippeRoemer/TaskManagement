ca2c766e17886704c1ff92b785ac87869a0366f5
Philippe Roemer <philipperoemer@yahoo.com>
2022-10-13 15:39:59 -0400

    Project error message and reset project toggle added

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -64,6 +64,7 @@ function Dashboard() {
             setCompletedTask(completedTasks);
         };
         getTasks();
+        setToggleAddProject(false);
     }, [selectedProjectID, updateList]);
 
     /* GET PROJECTS */
